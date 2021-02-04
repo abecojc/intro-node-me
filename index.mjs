5ef8a2e9db7c65a29593c34b401d45aa5e823a5a
Abeco Jean Claude <73258089+abecojc@users.noreply.github.com>
2021-02-04 13:46:26 -0500

    edit layout

--- index.mjs
+++ index.mjs
@@ -2,13 +2,12 @@ import { info } from "./info.js"
 
 const {name:{firstName, middleName, lastName}, location:{city,stateAbbr},
     skillsOfInterest:{webLanguages:{scripting,markdown,styling},
-        javascriptFrameworks:{title,popularUse}}, } = info
+        javascriptFrameworks:{title,popularUse}}, training:{school,address,parentOrg}} = info
 console.log(`My name is ${firstName} ${middleName} ${lastName}`)
 console.log(`From ${city} ${stateAbbr}`)
 console.log("---------------------")
-console.log(`The skills I am focused on are: ${firstName} ${middleName} ${lastName}`)
-console.log(`From ${city} ${stateAbbr}`)
+console.log(`The skills I am focused on are: ${scripting}, ${markdown} and ${styling}`)
+console.log(`Using ${title} in ${popularUse}`)
 console.log("---------------------")
-console.log(`My name is ${firstName} ${middleName} ${lastName}`)
-console.log(`From ${city} ${stateAbbr}`)
-console.log("---------------------")
+console.log(`I am a student at ${parentOrg}'s ${school} in ${address} `)
+console.log(`Thank you`)
