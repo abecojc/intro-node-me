77ed8e0629191c33e52bce1e6132cd0413cd38ef
Abeco Jean Claude <73258089+abecojc@users.noreply.github.com>
2021-02-10 10:41:32 -0500

    add Procfile

--- server.mjs
+++ server.mjs
@@ -2,7 +2,7 @@ import express from 'express';
 import { info } from './info.mjs';
 
 const app = express();
-const PORT = '3333';
+const PORT = process.env.PORT || '5000';
 
 app.get('/', ( req, res ) =>{
     res.send('This API contains my resume details including the areas of web development I am familiar with. You can run the command: [ npx abeco-resume ] to access my resume in the index.mjs file. Thank you.')
